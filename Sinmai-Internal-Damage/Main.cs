dfb1f99add78857e4a43220f4bd0913d21422059
Moe Kotoki <Kotoki1337@gmail.com>
2022-02-17 16:25:20 +0800

    Init

---
+++ Sinmai-Internal-Damage/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using UnityEngine;
+using Microsoft.Win32;
+
+namespace Sinmai_Internal_Damage
+{
+    public class Main
+    {
+        public static GameObject _objects;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        public void Init()
+        {
+            AllocConsole();
+            _objects = new GameObject();
+            _objects.AddComponent<Sinmai_GUI>();
+            UnityEngine.Object.DontDestroyOnLoad(_objects);
+            Console.Log("INJECT=-----------------------------------------");
+        }
+    }
+}
---
+++ Sinmai-Internal-Damage/Sinmai_GUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Sinmai_Internal_Damage
+{
+    public class Sinmai_GUI : MonoBehaviour
+    {
+        private Rect window;
+        bool menu_toggle;
+
+        public void Start()
+        {
+            // Rect(X, Y, W, H);
+            window = new Rect(10f,10f, 300f, 200f);
+        }
+
+        public void Update()
+        {
+           if(Input.GetKeyDown(KeyCode.Delete))
+            {
+                menu_toggle = !menu_toggle;
+            }
+        }
+
+        public static Color Color
+        {
+            get { return GUI.color; }
+            set { GUI.color = value; }
+        }
+
+        public static void DrawBox(Vector2 position, Vector2 size, bool centered = true)
+        {
+            var upperLeft = centered ? position - size / 2f : position;
+            GUI.DrawTexture(new Rect(position, size), Texture2D.whiteTexture, ScaleMode.StretchToFill);
+        }
+
+        public static void DrawBox(Vector2 position, Vector2 size, Color color, bool centered = true)
+        {
+            Color = color;
+            DrawBox(position, size, centered);
+        }
+
+        public void OnGUI()
+        {
+            DrawBox(new Vector2(Screen.width / 2f, Screen.height/ 2f), new Vector2(40, 40), Color.green);
+            Console.Log("OnGui Called");
+        }
+
+        public void Page1(int id)
+        {
+            // This button named by mlc.yaw
+            GUILayout.Button("nigger", new GUILayoutOption[0]);
+
+            GUILayout.Space(10f);
+            GUI.DragWindow();
+        }
+    }
+}
